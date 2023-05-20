0b417399d0043dc05f6773daa03f3e56fc65852a
litcoderrr@gmail.com <litcoderrr@gmail.com>
2023-05-21 00:19:16 +0100

    feat:refactored the homepgae

--- script.js
+++ script.js
@@ -6,8 +6,32 @@ function openNav() {
     document.getElementById("myNav").style.display= "none";
   }
   
-  
   window.addEventListener('load', function () {
-  const preloader = document.getElementById('preloader');
-  preloader.style.display = 'none';
+  const countdownElement = document.querySelector('.countdown');
+
+  const updateCountdown = function (percentage) {
+    countdownElement.textContent = `${percentage} / 100`;
+
+    if (percentage === 100) {
+      // Hide the preloader and show the website content
+      const preloader = document.getElementById('preloader');
+      preloader.style.display = 'none';
+      // Add any necessary code to show the website content
+    }
+  };
+
+  let percentage = 0;
+  const increment = 1;
+  const interval = 60; // in milliseconds
+
+  const countdownInterval = setInterval(function () {
+    percentage += increment;
+    if (percentage <= 100) {
+      updateCountdown(percentage);
+    } else {
+      clearInterval(countdownInterval);
+    }
+  }, interval);
 });
+
+  
